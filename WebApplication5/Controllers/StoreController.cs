793b73599428ef0d2a4296539bceb9b7e457cde4
Aditya Pratama <orochimadit@gmail.com>
2017-09-02 15:37:26 +0700

    first asp Project

---
+++ WebApplication5/Controllers/StoreController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+namespace WebApplication5.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            var genres = new List<Genre> {
+                new Genre  {Name="Jazz" },
+                new Genre  {Name="Pop" },
+                new Genre  {Name="Rock" }
+            };
+            return View(genres);
+        }
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre {Name=genre };
+            return View(genreModel);
+        }
+        public ActionResult Detail(int id)
+        {
+
+            var album = new Album { Tittle = "Album " + id };
+            return View(album);
+        }
+    }
+}
---
+++ WebApplication5/Models/Album.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    public class Album
+    {
+        public string Tittle { get; set; }
+        public Genre genre { get; set; }
+    }
+}
